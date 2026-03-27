726f76ca7c6acf5a6b0d12edc2846d854a701dcc
Robson Sandro <robsonsandro@hotmail.com>
2026-03-27 14:52:04 -0300

    comunicacao_com_back

--- src/components/features/LoginForm.tsx
+++ src/components/features/LoginForm.tsx
@@ -1,19 +1,49 @@
 "use client";
 import { VStack, Input, Button, Text, Box, Heading } from "@chakra-ui/react";
 import { useState } from "react";
+import {useRouter} from "next/navigation";
 
 
 export function LoginForm() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [carregando, setCarregando] = useState(false);
+    const router = useRouter();
 
     const handleLogin = async () => {
         setCarregando(true);
-        // Aqui você faria o fetch para o seu backend Java (ex: /api/auth/login)
-        console.log("Tentando logar com:", email, senha);
-        setCarregando(false);
+        try {
+            const response = await fetch("http://localhost:8080/api/usuarios/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    senha: senha,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+
+
+                localStorage.setItem("token", data.token);
+
+
+                router.push("/");
+            } else {
+                const errorMsg = await response.text();
+                alert(errorMsg || "Erro ao realizar login.");
+            }
+        } catch (error) {
+            console.error("Erro ao conectar com o servidor:", error);
+            alert("Servidor fora do ar!");
+        } finally {
+            setCarregando(false);
+        }
     };
+
 
     return (
         <Box p={8} bg="white" borderRadius="xl" shadow="lg" w="400px">
