034dade9b38bd1423f05809ffc5a0a2341375a2f
Robson Sandro <robsonsandro@hotmail.com>
2026-05-28 10:15:50 -0300

    testes_e2e_Cypress

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,44 @@
+describe('Fluxo de Autenticação - E2E Real', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/login')
+  })
+
+  it('Deve renderizar os elementos principais da tela', () => {
+    cy.contains('Seja Bem-vindo!').should('be.visible')
+    cy.get('input[type="email"]').should('be.visible')
+    cy.get('input[type="password"]').should('be.visible')
+    cy.get('button[type="submit"]').contains('Entrar').should('be.visible')
+  })
+
+  it('Deve fazer login com sucesso e ser redirecionado', () => {
+    cy.intercept('POST', '**/login').as('chamadaLogin')
+
+    cy.get('input[type="email"]').type('j@j.com')
+    cy.get('input[type="password"]').type('123')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('eq', 'http://localhost:3000/')
+
+    cy.window().then((win) => {
+      const token = win.localStorage.getItem('token');
+      expect(token).to.not.be.null;
+    })
+  })
+
+  it('Deve redirecionar para a tela de cadastro ao clicar no link', () => {
+    cy.contains('Cadastre-se aqui').click()
+    cy.url().should('include', '/cadastro')
+  })
+
+  it('Deve redirecionar para a recuperação de senha ao clicar no link', () => {
+    cy.get('form').should('be.visible')
+    cy.wait(500)
+    cy.contains('span', 'Trocar Senha')
+        .should('have.css', 'cursor', 'pointer')
+        .click({ force: true })
+
+    cy.url({ timeout: 6000 }).should('include', '/esquecisenha')
+    cy.contains('Recuperar Senha').should('be.visible')
+  })
+})
